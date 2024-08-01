193fca0176903d756d847f371c46eb6e76adc6e7
istasyi <istasi01@gmail.com>
2024-08-01 19:07:37 +0300

    completed def generate-students

--- HW_3/main.py
+++ HW_3/main.py
@@ -1,19 +1,30 @@
-import numpy as np
 import pandas as pd
+import requests
 from faker import Faker
-from flask import Flask, request, Response
+from flask import Flask, request, jsonify
+import csv
+
+
+import httpx
 
 app = Flask(__name__)
 fake = Faker()
 
+
 @app.route('/generate-students', methods=['GET'])
 def generate_students():
-    count = int(request.args.get('count', 100))
-    if count > 1000:
-        count = 1000
+    number_student = request.args.get("number_student", "100")
+
+    if not number_student.isdigit():
+        return "Error! Enter the number of students"
+
+    number_student = int(number_student)
+
+    if not 1 <= number_student <= 1000:
+        return "Error! Enter the number of students between 1 and 1000"
 
     students = []
-    for _ in range(count):
+    for _ in range(number_student):
         student = {
             'First Name': fake.first_name(),
             'Last Name': fake.last_name(),
@@ -25,14 +36,41 @@ def generate_students():
 
     df = pd.DataFrame(students)
     csv_file = 'students.csv'
-    df.to_csv(csv_file, index=False)
+    df.to_csv(csv_file)
 
-    with open(csv_file, 'r') as file:
-        csv_content = file.read()  # Read whole file
+    data = []
+    with open(csv_file, newline='') as csvfile:
+        csvreader = csv.DictReader(csvfile)
+        for row in csvreader:
+            data.append(row)
 
-    # Directly return csv data with text/plain mimetype
-    return Response(csv_content, mimetype='text/plain')
+    return jsonify(data)
 
+
+@app.route('/bitcoin-rate', methods=['GET'])
+def get_bitcoin_value():
+    currency = request.args.get('currency', 'USD')
+    count = int(request.args.get('count', 1))
+
+    response = requests.get('https://bitpay.com/api/rates')
+    rates = response.json()
+
+    for rate in rates:
+        if rate['code'] == currency:
+            bitcoin_value = rate['rate'] * count
+            return jsonify(
+                {"currency": currency, "symbol": get_currency_symbol(currency), "bitcoin_value": bitcoin_value})
+
+    return jsonify({"error": "Currency not found"}), 404
+
+
+def get_currency_symbol(currency_code):
+    symbols = {
+        "USD": "$",
+        "EUR": "€",
+        "UAH": "₴",
+    }
+    return symbols.get(currency_code, currency_code)
 
 if __name__ == '__main__':
     app.run(debug=True)
